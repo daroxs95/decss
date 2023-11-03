7ba50b4ed823bbe3b1997d68fb428f95b5b8ac04
Darián López Utra <daroxs95@gmail.com>
2023-11-03 04:06:14 -0400

    Add tabs functionality

---
+++ utils/tabs/index.mjs
@@ -0,0 +1,28 @@
+const allTabButtons = document.querySelectorAll(".tab-button");
+
+function openTab(tab) {
+  const tabButtons = tab.parentNode.querySelectorAll(".tab-button");
+  tabButtons.forEach((button) => {
+    button.classList.remove("active");
+    if (button === tab) {
+      button.classList.add("active");
+    }
+  });
+
+  const tabContents = tab.parentNode.parentNode.querySelectorAll(
+    ".tab-content"
+  );
+
+  tabContents.forEach((content) => {
+    content.classList.remove("active");
+    if (content.id === tab.dataset.tab) {
+      content.classList.add("active");
+    }
+  });
+}
+
+allTabButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    openTab(button);
+  });
+});
